35db4f207e800cde20b1a8eda8e257ac7c23fdc8
pether.te.z@hotmail.com <pether.te.z@hotmail.com>
2021-10-01 21:35:48 +0000

    add program generate a number and if followed by +, - , 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Generate a random number and and I print if the number
+ *        is positive, negative, or zero.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n == 0)
+		printf("%i is zero\n", n);
+	else
+		printf("%i is negative\n", n);
+
+	return (0);
+}
+
