a46bcd2d775de9a4fd33bf562424193c42667ef2
pether.te.z@hotmail.com <pether.te.z@hotmail.com>
2021-10-06 02:24:10 +0000

    add two files compiled recocgnice is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - print _putchar
+ * @c: The character to print
+ *
+ * Return: On success 1..
+ */
+int _islower(int c)
+{
+
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+		return (0);
+
+}
