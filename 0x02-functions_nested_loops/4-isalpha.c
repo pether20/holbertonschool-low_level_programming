e16ba9310f09764aa038f4e90ae6b1bd9328218a
pether.te.z@hotmail.com <pether.te.z@hotmail.com>
2021-10-06 04:26:26 +0000

    add two file compiled file 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - print _putchar
+ * @c: The character to print
+ *
+ * Return: On success 1..
+ */
+int _isalpha(int c)
+{
+
+	if (c >= 97 && c <= 122)
+		return (1);
+	else if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+
+}
