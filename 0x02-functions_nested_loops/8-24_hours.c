95ef22fb7509ce546316ab3f94d36414180b163d
pether.te.z@hotmail.com <pether.te.z@hotmail.com>
2021-10-06 08:43:46 +0000

    add two file compilation time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print _putchar
+ * @n: The inter to print
+ *
+ * Return: On success 1..
+ */
+void jack_bauer(void)
+{
+	int m = 0, n = 0, b = 0, a = 0;
+	int x = 9;
+
+	while (m <= 2)
+	{
+		while (n <= x)
+		{
+
+		while (b <= 5)
+		{
+
+		while (a <= 9)
+		{
+
+		_putchar('0' + m);
+		_putchar('0' + n);
+		_putchar(':');
+		_putchar('0' + b);
+		_putchar('0' + a);
+		_putchar('\n');
+		a++;
+		}
+
+		b++;
+		a = 0;
+		}
+
+		n++;
+		b = 0;
+		}
+
+		m++;
+		if (m == 2)
+		x = 3;
+
+		n = 0;
+
+	}
+
+}
