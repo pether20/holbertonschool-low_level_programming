d76bd9674646148d0c8c07543d077a4977fa8fda
pether.te.z@hotmail.com <pether.te.z@hotmail.com>
2021-10-06 04:48:23 +0000

    add two file compiled print the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - print _putchar
+ * @n: The inter to print
+ *
+ * Return: On success 1..
+ */
+int print_sign(int n)
+{
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
