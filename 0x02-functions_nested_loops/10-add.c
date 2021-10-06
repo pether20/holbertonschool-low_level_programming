dd81778875fd6be6b9f70ec8779fc36e13a33aae
pether.te.z@hotmail.com <pether.te.z@hotmail.com>
2021-10-06 18:59:50 +0000

    add file 10 compiled

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - return value r
+ * @a: The int
+ * @b: The int
+ *
+ * Return: On success r.
+ */
+int add(int a, int b)
+{
+
+	int r;
+
+	r = a + b;
+	return (r);
+
+}
