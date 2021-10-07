65ffc6db1c37924bc65d2faa0d99760afc7884fc
pether.te.z@hotmail.com <pether.te.z@hotmail.com>
2021-10-07 23:19:03 +0000

    add file compilation 0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ * @c: int value
+ *
+ * Return: Always 0.
+ */
+int _isupper (int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);_isupper
+}
