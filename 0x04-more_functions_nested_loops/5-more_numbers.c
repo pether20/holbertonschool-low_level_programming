5d8e54134e4a38e64bc7f6bf539016ab8304a4ad
pether.te.z@hotmail.com <pether.te.z@hotmail.com>
2021-10-08 02:26:59 +0000

    add two file compiled

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,13 +9,13 @@ void more_numbers(void)
 {
 	int a, i;
 
-	for (i = 0; a < 10; a++)
+	for (i = 0; i < 10; i++)
 	{
 		for (a = 0; a < 15; a++)
 		{
 			if (a >= 10)
 				_putchar('0' + (a / 10));
-			_putchar('0' + (a % 10))
+			_putchar('0' + (a % 10));
 		}
 	_putchar('\n');
 	}
