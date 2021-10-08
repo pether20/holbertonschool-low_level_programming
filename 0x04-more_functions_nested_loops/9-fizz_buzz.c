f30f3480a0c9b4dd322e6b8cdaf56668138a153f
pether.te.z@hotmail.com <pether.te.z@hotmail.com>
2021-10-08 18:14:23 +0000

    add file copilation print number from 0 at 100 whit conditions

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Print number from the 0 at 100.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else
+		{
+			if (i % 3 == 0)
+			printf("Fizz ");
+			else if (i % 5 == 0)
+			{
+				if (i == 100)
+				printf("Buzz\n");
+				printf("Buzz ");
+			}
+			else
+				printf("%d ", i);
+		}
+
+	}
+	return (0);
+}
