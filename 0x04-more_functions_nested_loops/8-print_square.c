191afbdf1d87d0577345c0c27d7526b024129393
pether.te.z@hotmail.com <pether.te.z@hotmail.com>
2021-10-08 05:42:46 +0000

    add description comment file compiled

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,18 +6,18 @@
  */
 void print_square(int size)
 {
+	int x, y;
+
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (i = 0; i < size; i++)
+		for (y = 1; y < size; y++)
 		{
-			for (j = 0; j < size; j++)
-			{
+			for (x = 1; x <= size; x++)
 				_putchar('#');
-			}
 			_putchar('\n');
 		}
 	}
