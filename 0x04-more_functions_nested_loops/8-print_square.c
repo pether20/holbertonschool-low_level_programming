9f5663ea135b0b1ab5742705d80a6cf744b07bf3
pether.te.z@hotmail.com <pether.te.z@hotmail.com>
2021-10-08 07:09:52 +0000

    add File print square with #

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: size
+ * Return: Void
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+		for (y = 1; y <= size; y++)
+		{
+			for (x = 1; x <= size; x++)
+			_putchar('#');
+		_putchar('\n');
+		}
+	}
+}
