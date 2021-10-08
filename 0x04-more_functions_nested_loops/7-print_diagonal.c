7edf7661a7deceede7c717be64e0085309b73920
pether.te.z@hotmail.com <pether.te.z@hotmail.com>
2021-10-08 04:52:16 +0000

    add description comment file compiled

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - check the code.
+ * @n: int value
+ * Return: Void.
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n < 0)
+		_putchar('\n');
+	else
+	{
+	for (x = 0; x <=  n; x++)
+	{
+		for (y = 1; y < x; y++)
+			_putchar(92);
+		_putchar(' ');
+		_putchar('\n')	
+	}
+	
+	}
+}
