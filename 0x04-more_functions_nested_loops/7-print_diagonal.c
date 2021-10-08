341aa89ee262937fbb23d62d2fccd35d6759c553
pether.te.z@hotmail.com <pether.te.z@hotmail.com>
2021-10-08 05:18:16 +0000

    add character \ en el putchar

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_diagonal - check the code.
+ * print_diagonal - print \ diagonale.
  * @n: int value
  * Return: Void.
  */
@@ -12,14 +12,14 @@ void print_diagonal(int n)
 	if (n < 0)
 		_putchar('\n');
 	else
-	{
-	for (x = 0; x <=  n; x++)
-	{
-		for (y = 1; y < x; y++)
-			_putchar(92);
-		_putchar(' ');
+		{
+		for (x = 0; x <=  n; x++)
+		{
+			for (y = 1; y < x; y++)
+			_putchar(' ');
+		_putchar(92);
 		_putchar('\n');
-	}
+		}
 
 	}
 }
