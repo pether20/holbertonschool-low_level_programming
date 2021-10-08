21ac5c66c6336c703efc30ef5c8ffb6ac0788068
pether.te.z@hotmail.com <pether.te.z@hotmail.com>
2021-10-08 01:35:26 +0000

    add two file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		_putchar('0' + a);
+	_putchar('\n');
+}
