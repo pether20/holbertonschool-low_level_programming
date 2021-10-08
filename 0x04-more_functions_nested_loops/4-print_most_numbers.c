0edab5791045a119935266076c71de9738727389
pether.te.z@hotmail.com <pether.te.z@hotmail.com>
2021-10-08 01:43:19 +0000

    add two file compiled

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		if (a != 2 || a != 4)
+		_putchar('0' + a);
+	}
+	_putchar('\n');
+}
