69270ebd475861c3ec950ab1ee762294eb783ce9
pether.te.z@hotmail.com <pether.te.z@hotmail.com>
2021-10-13 04:57:20 +0000

    add to file revert string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,14 +10,18 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (*(s + i) != '\0')
+	while (*s != '\0')
 	{
 		i++;
+		s++;
 	}
-	while (i < 0)
+	i--;
+	s--;
+	while (i >= 0)
 	{
-		_putchar(*(s + i));
 		i--;
+		_putchar(*s);
+		s--;
 	}
 	_putchar('\n');
 }
