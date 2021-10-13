64b25e9ef36718533af0f2ea06147cfcc2e1a89b
pether.te.z@hotmail.com <pether.te.z@hotmail.com>
2021-10-13 01:53:14 +0000

    add to file count long string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - print string
+ * @str: pointer char
+ *
+ * Return: Value
+ */
+void _strlen(char *str)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		_putchar(*(s + i));
+		i++;
+	}
+	_putchar('\n');
+}
