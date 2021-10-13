ed47cb2b195acd1ee7698b47d3eb20b466e2ff4d
pether.te.z@hotmail.com <pether.te.z@hotmail.com>
2021-10-13 17:27:19 +0000

    add to file revert string

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * print_array - printse
  * @a: pointer
@@ -7,25 +8,15 @@
  */
 void print_array(int *a, int n)
 {
-	int len = 0;
 	int i;
-	int *tmp;
 
-	tmp = a;
-	while (*a != 0)
-	{
-		len++;
-		a++;
-	}
-	a = tmp;
-	if (n > len)
-		n = len;
 	for (i = 0; i < n; i++)
 	{
-		printf("%i", a[i]);
-		if (!(i + 1 == n))
+		printf("%d", a[i]);
+		if (i == n - 1)
+			continue;
+
 		printf(", ");
-		else
-		printf("\n");
 	}
+	printf("\n");
 }
