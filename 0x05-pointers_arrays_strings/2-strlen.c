103670a8f29656d1673569e559c11e60fe968f18
pether.te.z@hotmail.com <pether.te.z@hotmail.com>
2021-10-13 01:30:36 +0000

    add to file count long string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int _strlen - leng string
+ * @s: pointer char
+ *
+ * Return: Value
+ */
+void int_strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
