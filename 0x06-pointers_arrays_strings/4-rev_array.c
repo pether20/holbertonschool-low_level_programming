c02a7a8e3e1783482ea2a2fdab5d8d52393fc797
pether.te.z@hotmail.com <pether.te.z@hotmail.com>
2021-10-15 04:28:13 +0000

    Add file revert strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array -  revert string
+ * @a: pointer s
+ * @n: int 
+ * Return: *a invert
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int temp;
+
+	while (i < n / 2)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+		i++;
+	}
+}
