07482573cff1f4077c35e6167396c2172db60cb8
pether.te.z@hotmail.com <pether.te.z@hotmail.com>
2021-10-15 04:37:21 +0000

    Add file convert uppercase strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,8 +12,8 @@ char *string_toupper(char *s)
 
 	while (s[i] != 0)
 	{
-	if (s[i] >= 'a' && s[i] <= 'z')
-		s[i] -= 32;
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
 		i++;
 	}
 	return (s);
