f16be37000d52a74fda5ad29f28847e5db1654c5
pether.te.z@hotmail.com <pether.te.z@hotmail.com>
2021-10-18 17:05:40 +0000

    add file compilation gets the length character in string

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *_strspn - long string
+ * @s: string
+ * @accept: character
+ *
+ * Return: pointer
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	unsigned int bytes = 0;
+
+	while (*s)
+	{
+	for (i = 0; accept[i]; i++)
+	{
+		if (*s == accept[i])
+		{
+		bytes++;
+		break;
+		}
+		else if (accept[i + 1] == '\0')
+			return (bytes);
+	}
+	s++;
+	}
+
+	return (bytes);
+}
