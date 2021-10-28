24c7e512a3607e6ecc4df031b3acba09d70486e8
pether20 <pether.te.z@hotmail.com>
2021-10-28 19:08:17 +0000

    add conditional if NULL in compilation 2 concatenate array new space memory

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,11 +14,20 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j, size;
 	char *new;
 
-	for (i = 0; s1[i] != '\0'; i++)
-	{}
-	for (j = 0; s2[j] != '\0'; j++)
-	{}
-
+	if (s1 != NULL)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+		{}
+	}
+	else
+		s1 = "";
+	if (s2 != NULL)
+	{
+		for (j = 0; s2[j] != '\0'; j++)
+		{}
+	}
+	else
+		s2 = "";
 	size = i + j + 1;
 	new = malloc(sizeof(char) * size);
 	if (new == NULL)
