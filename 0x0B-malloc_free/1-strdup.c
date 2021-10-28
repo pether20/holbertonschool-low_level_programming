361f39ca7c7ff7ff17406dcb898f4527d8c23434
pether20 <pether.te.z@hotmail.com>
2021-10-28 17:40:52 +0000

    add file compilation 1 duplicate array new space memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_strdup - create new array copy
+ * @str: string
+ *
+ *
+ * Return: new.
+ */
+char *_strdup(char *str)
+{
+	unsigned int i, size;
+	char *new;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (size = 0; str[size] != '\0'; size++)
+	{}
+
+	size += 1;
+	new = malloc(sizeof(char) * size);
+	for (i = 0; str[i] != '\0'; i++)
+		new[i] = str[i];
+
+	new[size - 1] = '\0';
+	return (new);
+}
