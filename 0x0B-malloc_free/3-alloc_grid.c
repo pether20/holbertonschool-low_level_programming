e6e8c18f842de16dc09d326994c9da1ced56cf50
pether20 <pether.te.z@hotmail.com>
2021-10-28 23:43:45 +0000

    add compilation file 3 create array dimensional

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,13 +25,6 @@ int **alloc_grid(int width, int height)
 	for (y = 0; y < height; y++)
 	{
 		grid[y] = malloc(sizeof(int) * width);
-		if (grid[y] == NULL)
-		{
-			for (x = y - 1; x > 0; x--)
-				free(grid[x]);
-			free(grid);
-			return (NULL);
-		}
 		for (x = 0; x < width; x++)
 			grid[y][x] = 0;
 	}
