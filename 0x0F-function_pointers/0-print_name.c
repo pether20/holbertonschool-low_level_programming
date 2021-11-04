1845c27c727b5f6e81353140a5724d5455120b91
pether20 <pether.te.z@hotmail.com>
2021-11-04 02:57:44 +0000

    add file Function of the Pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - A new type describing a dog.
+ * @name: The name of the dog.
+ * @f: The age of the dog.
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
