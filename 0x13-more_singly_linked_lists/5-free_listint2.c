0fa772b18bfafc562f8fac62be9eaa5453098f3f
root <root@LAPTOP-QALOEB9N.localdomain>
2021-11-16 18:58:28 -0500

    Add function that frees a listint_t list and sets the head to NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,11 +11,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
+	if (head == NULL)
+		return;
+
 	while (*head)
 	{
 		tmp = (*head)->next;
 		free(*head);
 		*head = tmp;
 	}
-	head = NULL;
+	*head = NULL;
 }
