6c91fc620b963b18ffc56c041ab2a7550c5c9f0f
root <root@LAPTOP-QALOEB9N.localdomain>
2021-11-16 15:01:47 -0500

    Add function that frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * add_nodeint - add the elements of a list_t list.
+ * @head: list_t list.
+ *
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+	return (*head);
+}
