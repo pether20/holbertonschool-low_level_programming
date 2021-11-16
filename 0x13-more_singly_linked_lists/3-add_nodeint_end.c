cd976ff8db8c23842b7ddd85e4f0c86aac64bb9d
root <root@LAPTOP-QALOEB9N.localdomain>
2021-11-16 13:35:38 -0500

    Add a script function that add nodes in the end list a listint_t listint

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * add_nodeint - add the elements of a list_t list.
+ * @head: list_t list.
+ * @n: integer n.
+ * Return: Direction of nodes.
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new, *sig;
+
+	new = malloc(sizeof( listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+
+	else
+	{
+		sig = *head;
+		while (sig->next != NULL)
+			sig = sig->next;
+
+		sig->next = new;
+	}
+	return (*head);
+}
