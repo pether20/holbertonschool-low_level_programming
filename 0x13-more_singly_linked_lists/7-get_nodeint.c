d8f14abdf491aab21a5825f2b3a6671397253208
root <root@LAPTOP-QALOEB9N.localdomain>
2021-11-16 20:45:34 -0500

    Add function that return node position n in a listint_t listint

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,14 +14,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *tmp = head;
 	unsigned int node = 0;
 
-	if (index == 0)
+	if (head == NULL)
 	{
-		tmp->next = head;
-		head = tmp;
-		return (tmp);
+		return (NULL);
 	}
 
-	for (node = 0; node < index; node++)
+	for (node = 0; tmp != NULL && node < index; node++)
 	{
 		if (tmp->next == NULL)
 			return (NULL);
