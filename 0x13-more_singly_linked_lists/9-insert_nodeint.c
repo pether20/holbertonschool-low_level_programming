9225653b899ba0a1f6fef44962fcc87519a4eee6
root <root@LAPTOP-QALOEB9N.localdomain>
2021-11-16 20:27:17 -0500

    Add function that inserts a new node at a given position in the data (n) of a listint_t

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * insert_nodeint_at_index - add the node in position idx of a listint_t list.
+ * @head: listint_t listint.
+ * @idx: index node.
+ * @n: integer n.
+ * Return: Direction of nodes.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *tmp = *head, *tmp2;
+	unsigned int i;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	if (idx == 0)
+	{
+		new->next = NULL;
+		*head = new;
+		return (new);
+	}
+	for (i = 0; i + 1 < idx; i++)
+	{
+		if (tmp->next == NULL)
+			return (NULL);
+		tmp = tmp->next;
+	}
+	tmp2 = tmp->next;
+	tmp->next = new;
+	new->next = tmp2;
+
+	return (new);
+}
