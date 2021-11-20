ec42cad078a033cdb4714d7493eed05dd4c1aaab
root <root@LAPTOP-QALOEB9N.localdomain>
2021-11-19 21:21:35 -0500

    add function  converts a binary number to an unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: is pointing to a string of 0 and 1 chars
+ *
+ * Return: 1 if everything is fine otherwise 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+        unsigned int binary = 0;
+
+        if (!b)
+                return (0);
+
+        while (*b)
+        {
+                if (*b != 48 && *b != 49)
+                        return (0);
+                binary = binary << 1;
+                if (*b == 49)
+                        binary = binary ^ 1;
+                b++;
+        }
+        return (binary);
+}
