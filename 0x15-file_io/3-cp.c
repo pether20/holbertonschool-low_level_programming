b658a7ecf54e4356a15561e894858ce1ea882015
root <root@LAPTOP-QALOEB9N.localdomain>
2021-11-25 11:37:50 -0500

    Add a program that copies the content of a file to another file.

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+void buf(int fd1, int fd2, char **av);
+
+/**
+ * main - copy file and other file
+ * @ac: pointer file
+ * @av: number od letters
+ * Return: Return succees or fail
+ */
+int main(int ac, char **av)
+{
+	int fd1, fd2;
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd1 = open(av[1], O_RDONLY);
+	if (fd1 == -1)	
+	{	dprintf(STDERR_FILENO, "Error: Can't read to %s\n", av[1]);
+		exit(98);
+	}
+	fd2 = open(av[2], O_CREAT | O_WRONLY | O_APPEND | O_TRUNC, 0664); 
+	if (fd2 == -1)
+	{       dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(98);
+	}
+	buf(fd1, fd2, av);
+	if (close(fd1) == -1 || close(fd2) == -1)
+		 dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE\n");
+	return (0);
+}
+/**
+  4  * main - copy file and other file
+  5  * @ac: pointer file
+  6  * @av: number od letters
+  7  * Return: Return succees or fail
+  8  */
+void buf(int fd1, int fd2, char **av)
+{
+	char buf[1024];
+	int MAX_BUF = 1024, r = 0, w;
+	
+	while(1)
+	{
+		 r = read(fd1, buf, MAX_BUF);
+		 if (r == 0)
+			 break;
+		 if (r == -1)
+		 {
+			  dprintf(STDERR_FILENO, "Error: Can't read to %s\n", av[1]);
+			  exit(98);
+		 }
+		 w = write(fd2, buf, r);
+		 if (w == -1)
+		 {
+			 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+		 }
+	}
+}
